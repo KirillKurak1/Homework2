ed2a50328f1c2d7f6856d294d77f65dca7fc5714
Kirill <107797389+KirrillKurak@users.noreply.github.com>
2023-11-08 19:12:55 +0300

    Changes

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -21,59 +21,9 @@ export default function Cards(props){
              {props.case.text}
             </p>
             
-        </div>,
+        </div>
 
-        <div className={props.case.color}>
-     <p className="">
-             {props.case.name_1}
-            </p>
-            <p className="">
-             {props.case.info_1}
-            </p>
-            <p className="">
-             {props.case.info_month_1}
-            </p>
-            <p className="">
-             {props.case.speed_1}
-            </p>
-            <p className="">
-             {props.case.text}
-            </p>
-        </div>,
-          <div className={props.case.color}>
-          <p className="">
-                  {props.case.name_2}
-                 </p>
-                 <p className="">
-                  {props.case.info_1}
-                 </p>
-                 <p className="">
-                  {props.case.info_month_2}
-                 </p>
-                 <p className="">
-                  {props.case.speed_2}
-                 </p>
-                 <p className="">
-                  {props.case.text}
-                 </p>
-             </div>,
-             <div className={props.case.color}>
-             <p className="">
-                     {props.case.name_3}
-                    </p>
-                    <p className="">
-                     {props.case.info_1}
-                    </p>
-                    <p className="">
-                     {props.case.info_month_3}
-                    </p>
-                    <p className="">
-                     {props.case.speed_3}
-                    </p>
-                    <p className="">
-                     {props.case.text}
-                    </p>
-                </div>
+       
 
 
 )
